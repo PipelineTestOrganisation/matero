cdad27199ce0007fe5e01d949b912b93dde93d6d
robbedb12 <92116505+robbedb12@users.noreply.github.com>
2025-03-03 15:25:58 +0100

    Update testttt.ps1

--- testttt.ps1
+++ testttt.ps1
@@ -1,15 +1,28 @@
 $BackupPath = "C:\iisServer\angular-27-02-2025"
 $NewPath = "C:\iisServer\angular\"
 
-$duplicateItems = Get-Item "$BackupPath*"
-if ($duplicateItems) {
-    $measure = $duplicateItems | Measure-Object
-    for ($i = $measure.Count -1 ; $i -ge 0; $i--) {
-        $duplicateItems[$i]
-        Rename-Item -Path $duplicateItems[$i].FullName -NewName "$BackupPath ($($i + 1 ))"
+if(Test-Path $BackupPath){
+    $duplicateItems = Get-Item "$BackupPath*"
+    if ($duplicateItems) {
+        $measure = $duplicateItems | Measure-Object
+        Write-Output "Count: $($measure.Count)"
+        for ($i = $($measure.Count -1); $i -ge 0; $i--) {
+            Write-Output "Index: $i"
+            if ($duplicateItems[$i].FullName) {
+                Write-Output "FullName: $($duplicateItems[$i].FullName)"
+                Rename-Item -Path $duplicateItems[$i].FullName -NewName "$BackupPath ($($i + 1))"
+            } else {
+                Write-Output "FullName is null for index $i"
+            }
+        }
     }
 }
 
-Move-Item -Path $NewPath -Destination $BackupPath
+
+if(Test-Path $NewPath ){
+    Move-Item -Path $NewPath -Destination $BackupPath
+}
 Copy-Item -r dist\test\browser\* $NewPath
-Copy-Item "$BackupPath\web.config" $NewPath -ErrorAction SilentlyContinue
+if(Test-Path $BackupPath){
+    Copy-Item "$BackupPath\web.config" $NewPath
+}
