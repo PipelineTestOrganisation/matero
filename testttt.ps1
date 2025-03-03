98437d3577484e268858cc88d663f6b43e7bb2a2
robbedb12 <92116505+robbedb12@users.noreply.github.com>
2025-03-03 14:41:35 +0100

    41

--- testttt.ps1
+++ testttt.ps1
@@ -1,13 +1,14 @@
 $BackupPath = "C:\iisServer\angular-27-02-2025"
-$NewPath = "C:\iisServer\angular"
+$NewPath = "C:\iisServer\angular\"
 
 $duplicateItems = Get-Item "$BackupPath*"
 if ($duplicateItems) {
-    $measure = Measure-Object -InputObject $duplicateItems
-    Write-Output $duplicateItems
+    $measure = $duplicateItems | Measure-Object
+    Write-Output $duplicateItems.Count
+    write-output $measure
     for ($i = $measure.Count; $i -ge 0; $i--) {
-        if ($null -ne $duplicateItems[$i]) {
-            $duplicateItems[$i].name
+        if ($null -ne $duplicateItems[$i-1]) {
+            $duplicateItems[$i-1].name
             Rename-Item -Path $duplicateItems[$i].FullName -NewName "$BackupPath ($($i + 1))"
         }
     }
