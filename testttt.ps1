a7b8c1da9e5987f46342db7280562c089470ec1e
robbedb12 <92116505+robbedb12@users.noreply.github.com>
2025-03-05 14:05:53 +0100

    2

--- testttt.ps1
+++ testttt.ps1
@@ -1,38 +1,38 @@
 $pathMainFolder = "C:\iisServer"
 $nameProject = "angular"
 
-$regexFolderCheck = "$nameProject-\d{4}-\d{2}-\d{2}( \(\d+\))?$"  
+$regexFolderCheck = "$nameProject-\d{4}-\d{2}-\d{2}( \((\d{0,2})\))?$"  
 
 $NewPath = "$pathMainFolder\$nameProject"
 $BackupPath = "$newPath-$(Get-Date -Format "yyyy-MM-dd")"
-$BackupPath = "$newPath-2025-03-01"
 $directories = Get-ChildItem -Path $pathMainFolder -Directory | Where-Object { $_.Name -match $regexFolderCheck }
 
-
-if($directories.Count -gt 3){
-        $directories | Sort-Object -Property LastWriteTime | Select-Object -First ($directories.Count - 2) | Remove-Item -Recurse
+if ($directories.Count -gt 3) {
+    $directories | Sort-Object -Property LastWriteTime | Select-Object -First 1 | Remove-Item -Recurse
 }
 
+$directories = Get-ChildItem -Path $pathMainFolder -Directory | Where-Object { $_.Name -match $regexFolderCheck }
 
-if(Test-Path $BackupPath){
-    $duplicateItems = $directories | Where-Object { $_.Name -like $BackupPath }
+if (Test-Path $BackupPath) {
+
+    $duplicateItems = $directories | Where-Object { $_.FullName -match [regex]::Escape($BackupPath) + "(\s\(\d+\))?$" }
     if ($duplicateItems) {
         $measure = $duplicateItems | Measure-Object
-        for ($i = $($measure.Count -1); $i -ge 0; $i--) {
-            
-            if ($duplicateItems[$i].FullName) {
+        for ($i = $($measure.Count - 1); $i -ge 0; $i--) {
+            if ($duplicateItems[$i].FullName -and $duplicateItems[$i].FullName -ne "$BackupPath ($($i + 1))") {
                 Rename-Item -Path $duplicateItems[$i].FullName -NewName "$BackupPath ($($i + 1))"
             }
         }
     }
 }
 
-if(Test-Path $NewPath ){
+if (Test-Path $NewPath ) {
     Move-Item -Path $NewPath -Destination $BackupPath
 }
 
 New-Item -ItemType Directory -Path $NewPath
 Copy-Item -Recurse -Path C:\GitProjects\matero\test\dist\test\browser\* -Destination $NewPath
-if(Test-Path "$BackupPath\web.config"){
+if (Test-Path "$BackupPath\web.config") {
     Copy-Item "$BackupPath\web.config" "$NewPath\web.config"
 }
+
